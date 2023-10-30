c651bc41cb747a6528b132a42cb7829cf9f454d1
Natru Swamy <nswamy@digitalguardian.com>
2023-10-30 06:00:22 +0000

    sum array

--- src/Ch01-Arrays/01_02/end/sum-array.cpp
+++ src/Ch01-Arrays/01_02/end/sum-array.cpp
@@ -4,10 +4,18 @@ using namespace std;
 int calculateSum(const int arr[], size_t size)
 {
     //...TODO: implement
+    if (size == 0) return 0;
+
+    int sum = 0;
+
+    for (size_t i=0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum;
 }
 
 int main()
 {
-    const int ints[]{1, -7, 17};
-    cout << "The sum of all elements in the array is " << calculateSum(ints, 3) << endl;
+    const int ints[]{1};
+    cout << "The sum of all elements in the array is " << calculateSum(ints, 1) << endl;
 }
